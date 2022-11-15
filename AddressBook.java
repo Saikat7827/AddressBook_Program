94abacdb7d49d692ab9d75e4ea627c2561914176
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-11-16 00:07:31 +0530

    Added : Contact details using user input

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,14 @@
 package com.bridgelabz;
 
 public class AddressBook {
-	public static void main(String[] args) {
-		System.out.println(" Welcome to Addressbook Program ");
-	}
+	 public static void main(String[] args) {
+		  System.out.println("--Welcome to Address Book Program--");
+
+		  Contacts c = new Contacts();
+		  c.getInputs();
+
+		  System.out.println("Here's what was entered: " + c.fName + " " + c.lName + " " + c.address + " " + c.city + " "
+		    + c.state + " " + c.email + " " + c.zip + " " + c.phNum);
+
+		  }
 }
